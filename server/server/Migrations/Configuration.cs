360c9d62303990e35294041de6d9122e8f6fd469
Roberto Sanchez <rsanchez92@live.com>
2018-02-03 22:41:49 -0800

    Finally be able to push to Azure db

---
+++ server/server/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace server.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<server.Models.SchoolContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(server.Models.SchoolContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
