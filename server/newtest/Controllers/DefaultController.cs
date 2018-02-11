0e647631ab59b3f987b907b7248d09ba5e27be3b
DPmax <Longcheng.Chi@student.csulb.edu>
2018-02-10 16:18:51 -0800

    myownservertest

---
+++ server/newtest/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace newtest.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            try
+            {
+                return Request.CreateResponse(HttpStatusCode.Found);
+            }
+            catch (Exception)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "No data found");
+            }
+        }
+    }
+}
