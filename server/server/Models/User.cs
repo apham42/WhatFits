bab303d16eeae011a2f5e0e79b696af0731f9e76
Roberto Sanchez <rsanchez92@live.com>
2018-02-13 14:39:24 -0800

    Updated Web.config with connection string

---
+++ server/server/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace server.Models
+{
+    public class User
+    {
+        public User()
+        {
+
+        }
+        [Key]
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        // The Following line is being debated by team
+       // public User[] Followers { get; set; }
+       public ICollection<User> Followers { get; set; }
+    }
+}
