782af7051c818cc7d6e032295b26a0ca2e2eb7ce
Roberto Sanchez <rsanchez92@live.com>
2018-02-18 16:17:21 -0800

    Unit Testing
    Creating unit test for user model. Inb4 SYSTEMFILES

---
+++ server/Gateways.UnitTests/UserControllerUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using server.Controllers;
+namespace Gateways.UnitTests
+{
+    
+    public class User
+    {
+        public int UserID;
+        public string Email;
+        public string FirstName;
+        public string LastName;
+        public string Gender;
+    }
+    
+    [TestClass]
+    public class UserControllerUnitTest
+    {
+        [TestMethod]
+        public void TestCreate()
+        {
+            //   Assert.AreEqual("HomeController","HomeController");
+            UserController controllerUnderTest = new UserController();
+            User usr = new User();
+            usr.UserID = 777;
+            usr.Email = "asdf@live.com";
+            usr.FirstName = "TestUser";
+            usr.LastName = "TestUserLastName";
+            usr.Gender = "Male";
+            string output = JsonConvert.SerializeObject(usr);
+            var result = controllerUnderTest.Create() as ViewResult;
+            Assert.AreEqual("fooview",result.ViewName);
+        }
+    }
+}
