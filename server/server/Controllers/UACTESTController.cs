bbdc41deaadcfe7014cad54e961573f115be3c58
apham42 <aphambi@gmail.com>
2018-03-07 19:06:12 -0800

    UAC JsonWebToken 	Create JWT for testing 	Create new proj for that ToDo: 	read JWT 	-> Get username claim 	-> get claims for Db 	-> create principal in Claims Transformer 	-> run through authorizationManager

---
+++ server/server/Controllers/UACTESTController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using Whatfits.UserAccessControl.Auth;
+using Whatfits.JsonWebToken.Controller;
+
+
+
+
+
+namespace server.Controllers
+{
+    public class UACTESTController : ApiController
+    {
+
+        [HttpPost]
+        [TokenAuthorize(claimType = "WORKOUT_ADD", claimValue = "ADD")]
+        public string TEST()
+        {
+            var re = Request;
+            var header = re.Headers;
+
+            string hey = header.GetValues("Token").First();
+            
+            return hey;
+        }
+
+        [HttpGet]
+        public string createToken()
+        {
+            return CreateJWT.CreateJsonWebToken();
+        }
+    }
+}
