2e927487f5c8467ffe5b79dc7c756b0b2a760828
Abram Villanueva <apvillan00@gmail.com>
2018-03-12 17:24:53 -0700

    Adding Registration files from MVC

---
+++ server/server/Controllers/SignUpController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using server.Services;
+
+namespace server.Controllers
+{
+    public class SignUpController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Register([FromBody] string username, string password)
+        {
+            AccountService service = new AccountService();
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return Ok();
+        }
+    }
+}
