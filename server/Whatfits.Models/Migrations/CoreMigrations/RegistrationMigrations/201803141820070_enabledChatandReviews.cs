21bc70221108f54d3bca3182175dacee2a1ab173
Roberto Sanchez <rsanchez92@live.com>
2018-03-14 11:32:22 -0700

    Retested Current Sprint Models

---
+++ server/Whatfits.Models/Migrations/CoreMigrations/RegistrationMigrations/201803141820070_enabledChatandReviews.cs
@@ -0,0 +1,19 @@
+namespace Whatfits.Models.Migrations.CoreMigrations.RegistrationMigrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class enabledChatandReviews : DbMigration
+    {
+        public override void Up()
+        {
+            
+            
+        }
+        
+        public override void Down()
+        {
+            
+        }
+    }
+}
