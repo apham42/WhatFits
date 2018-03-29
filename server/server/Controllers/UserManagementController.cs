e9b408bbb2bdff739c299e8a717399414e2f2e42
Roberto Sanchez <rsanchez92@live.com>
2018-03-28 20:40:54 -0700

    Created Controller and Service
    Created UserManagement Controller and Service module.

---
+++ server/server/Controllers/UserManagementController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using server.Services;
+namespace server.Controllers
+{
+    public class UserManagementController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult ManageUser(string userCred)
+        {
+            UserManagementService service = new UserManagementService();
+            if(userCred == null)
+            {
+                return Content(HttpStatusCode.BadRequest, "What");
+            }
+            return Ok("You reached the end of the controller");
+            
+        }
+    }
+}
---
+++ server/server/Services/UserManagementService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace server.Services
+{
+    public class UserManagementService
+    {
+        public  Boolean FindUser(string potentialUser)
+        {
+
+            return false;
+        }
+    }
+}
