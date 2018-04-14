3d81e241553ca3962da1cd28a7135ed18c22247c
Roberto Sanchez <rsanchez92@live.com>
2018-04-14 14:15:09 -0700

    Reorganized server side project
    Created BusinessLogic folder to contain all validations and services for this project. Deleted unecessary files from models. Combined all tests into one test project.

---
+++ server/Unit Tests/UserAccessControl/ControllerTest/AuthorizeTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading;
+using System.Web.Http.Controllers;
+using Whatfits.UserAccessControl.Controller;
+using Whatfits.UserAccessControl.Test.Constant;
+using Xunit;
+
+namespace Whatfits.UserAccessControl.Test.ControllerTest
+{
+    public class AuthorizeTest : AuthorizePrincipal
+    {
+        AuthorizePrincipal authorizePrincipal = new AuthorizePrincipal();
+        ClaimsPrincipalMockData mockData = new ClaimsPrincipalMockData();
+        HttpActionContext actionContext = new HttpActionContext();
+
+        [Fact]
+        public void NullCurrentPrincipal()
+        {
+            Thread.CurrentPrincipal = null;
+
+            authorizePrincipal.OnAuthorization(actionContext);
+
+            Assert.Equal(HttpStatusCode.Unauthorized, actionContext.Response.StatusCode);
+        }
+
+        [Fact]
+        public void InValidCurrentPrincipal()
+        {
+            authorizePrincipal.type = "WORKOUT_ADD";
+            authorizePrincipal.value = "Add";
+
+            Thread.CurrentPrincipal = mockData.PrincipalHasClaim();
+
+            authorizePrincipal.OnAuthorization(actionContext);
+
+            Assert.Equal(HttpStatusCode.Unauthorized, actionContext.Response.StatusCode);
+        }
+    }
+}
---
+++ server/server/Business Logic/Validators/Account Validator/SecurityQuestionValidator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluentValidation;
+using server.Model.Account;
+using server.Constants;
+
+namespace server.Model.Validators.Account_Validator
+{
+    /// <summary>
+    /// Validates the security questions and answers based on business rules
+    /// </summary>
+    public class SecurityQuestionValidator : AbstractValidator<List<SecurityQuestion>>
+    {
+        public SecurityQuestionValidator()
+        {
+            When(SecurityQuestions => SecurityQuestions != null, () =>
+            {
+                RuleFor(SecurityQuestions => SecurityQuestions.Count).Must(SecurityQuestionCount => SecurityQuestionCount == 3).WithMessage(AccountConstants.QUESTION_AMOUNT_ERROR);
+                RuleFor(SecurityQuestions => SecurityQuestions).Must(CheckAnswers).WithMessage(AccountConstants.ANSWER_INVALID_ERROR);
+            });
+        }
+
+        /// <summary>
+        /// Checks user input of the answers to the security questions
+        /// </summary>
+        /// <param name="securityQuestions"></param>
+        /// <returns> status of the validation of the answers </returns>
+        public bool CheckAnswers(List<SecurityQuestion> securityQuestions)
+        {
+            foreach (SecurityQuestion question in securityQuestions)
+            {
+                if (question.Answer == null || question.Answer.Equals("") || question.Answer.Length > 150)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
