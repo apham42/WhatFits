838f0c391155ef666e47356bfafba1bce626cadf
Unknown <aphambi@gmail.com>
2018-04-17 12:26:06 -0700

    Navbar/Vuex
    Login UI stuff and Vuex

--- client/src/components/Auth/LoginModal.vue
+++ client/src/components/Auth/LoginModal.vue
@@ -1,22 +1,51 @@
 <template>
-  <div></div>
+  <div class="modal is-active">
+    <div class="modal-background" @click="closeModal"></div>
+    <div class="modal-content">
+      <div class="box">
+        <img src="../../assets/Images/NavbarLogo/navbarLogo.png" alt="Go to Whatfits Home">
+        <div class="field">
+          <p class="control has-icons-left">
+            <input class="input" type="text" placeholder="Username">
+            <span class="icon is-small is-left">
+              <i class="fa fa-user"></i>
+            </span>
+          </p>
+        </div>
+        <div class="field">
+          <p class="control has-icons-left">
+            <input class="input" type="password" placeholder="Password">
+            <span class="icon is-small is-left">
+              <i class="fa fa-lock"></i>
+            </span>
+          </p>
+        </div>
+        <a class="button is-primary">Login</a>
+        <a class="button" @click="closeModal">Cancel</a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HomeNavBar from '@/components/NavBar/HomeNavBar.vue'
 export default {
   name: 'LoginModal',
   components: {
-    'HomeNavBar': HomeNavBar
   },
   data () {
     return {
-      showLogin: false
+    }
+  },
+  methods: {
+    closeModal: function () {
+      this.$store.dispatch('controlPopup', {modalStatus: false})
     }
   }
 }
 </script>
 
 <style>
-
+box {
+  position: absolute;
+}
 </style>
