09466cc9c08a29ac99008d88b4e4abf36f90bee4
Robby <longcheng.chi@student.csulb.edu>
2018-04-17 18:34:16 -0700

    Revert "Merge branch 'Chat'"
    This reverts commit 56b2e2e5d549e4b245e1bbdebf712519a2ec0083.

--- client/src/store.js
+++ client/src/store.js
@@ -1,20 +1,20 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+// import Vue from 'vue'
+// import Vuex from 'vuex'
 
-Vue.use(Vuex)
+// Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-    username: []
-  },
-  getters: {
-    users: function (state) {
-      return state.username
-    }
-  },
-  mutations: {
-    addUser: function (payload) {
-      this.username.push(payload)
-    }
-  }
-})
+// export default new Vuex.Store({
+//   state: {
+//     username: []
+//   },
+//   getters: {
+//     users: function (state) {
+//       return state.username
+//     }
+//   },
+//   mutations: {
+//     addUser: function (payload) {
+//       this.username.push(payload)
+//     }
+//   }
+// })
--- server/server/Controllers/ChatController.cs
+++ server/server/Controllers/ChatController.cs
@@ -12,7 +12,7 @@ using server.Business_Logic.Services;
 namespace server.Controllers
 {
     /// <summary>
-    /// Provides APIs for Chat Clientside
+    /// Provides APIs for Chat for Clientside
     /// </summary>
     [RoutePrefix("v1/chat")]
     public class ChatController : ApiController
@@ -60,6 +60,102 @@ namespace server.Controllers
             }
             return new HttpResponseMessage(HttpStatusCode.BadRequest);
         }
+        /// <summary>
+        /// Provides websocket handler when chat connection called
+        /// </summary>
+        //class ChatHandler : WebSocketHandler
+        //{
+        //    private ChatGateway mychat = new ChatGateway();
+        //    private static WebSocketCollection _chatUser = new WebSocketCollection();
+        //    // user list for feature friends list
+        //    private static List<string> friends = new List<string>();
+        //    private string connectedUser;
+        //    private byte[] _key;
+        //    private byte[] _iv;
+        //    /// <summary>
+        //    /// ChatHandler initialization
+        //    /// </summary>
+        //    public ChatHandler(string username, byte[] key, byte[] iv)
+        //    {
+        //        connectedUser = username;
+        //        if (!friends.Contains(connectedUser))
+        //            // add new user to user list
+        //            friends.Add(connectedUser);
+        //        _key = key;
+        //        _iv = iv;
+        //    }
+        //    /// <summary>
+        //    /// Provides connection functionality
+        //    /// Send back friends list and secrete key and initial value for encryption and decryption
+        //    /// </summary>
+        //    public override void OnOpen()
+        //    {
+        //        // check duplicate username
+        //        if (!_chatUser.Contains(this))
+        //            _chatUser.Add(this);
+        //        // prepare send back string message
+        //        var friends_info = "";
+
+        //        // add friends
+        //        int length = friends.Count;
+        //        for (int i = 0; i < length; i++)
+        //        {
+        //            if (i > 0)
+        //                friends_info += "," + friends[i];
+        //            else
+        //                friends_info += friends[i];
+        //        }
+        //        // for sending key
+        //        for (int j = 0; j < _key.Length; j++)
+        //        {
+        //            friends_info += "," + _key[j];
+        //        }
+        //        // for sending iv
+        //        for (int k = 0; k < _iv.Length; k++)
+        //        {
+        //            friends_info += "," + _iv[k];
+        //        }
+        //        // send friends information, key and initial value to connected user via websocket
+        //        _chatUser.Broadcast(JsonConvert.SerializeObject(friends_info));
+        //    }
+        //    /// <summary>
+        //    /// Provides receive message from server
+        //    /// Send back 
+        //    /// </summary>
+        //    public override void OnMessage(string message)
+        //    {
+        //        var ser = new JavaScriptSerializer();
+        //        var deser = ser.Deserialize<Message>(message);
+        //        // get the index of receiver 
+        //        //var index = friends.IndexOf(deser.UserName);
+        //        // send to receiver
+        //        if(!friends.Contains(deser.UserName))
+        //        {
+        //            Send(JsonConvert.SerializeObject("server"+ " said: " + "user-is-offline" + "  " + DateTime.Now.ToLocalTime()));
+        //        }
+        //        else
+        //        {
+        //            // type cast fine receiver's username in the websocket collection
+        //            _chatUser.SingleOrDefault(r => ((ChatHandler)r).connectedUser == deser.UserName).Send(JsonConvert.SerializeObject(connectedUser + " said: " + deser.MessageContent + "  " + DateTime.Now.ToLocalTime()));
+        //        }
+        //    }
+        //    /// <summary>
+        //    /// Error handler
+        //    /// </summary>
+        //    public override void OnError()
+        //    {
+        //        base.OnError();
+        //    }
+        //    /// <summary>
+        //    /// Provides disconnection functionality
+        //    /// remove connected user from websocket user collection
+        //    /// </summary>
+        //    public override void OnClose()
+        //    {
+        //        _chatUser.Remove(this);
+        //        friends.Remove(this.connectedUser);
+        //    }
+        //}
     }
 
     
