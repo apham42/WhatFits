5ac6c814bf990f6ae1d1bd314422fc3ec9f0c4ce
Unknown <aphambi@gmail.com>
2018-04-17 18:56:27 -0700

    Revert "Merge branch 'Chat'"
    This reverts commit a6ab82c3f3f466b3fc74d0de0fdc91fa507f3a53.

---
+++ server/server/Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using server.Model.Account;
+using System.Web.Http;
+
+namespace server.Controllers
+{
+    [RoutePrefix("v1/login")]
+    public class LoginController : ApiController
+    {
+        
+        //public IHttpActionResult Login([FromBody] UserCredential userCredential)
+        //{
+        //    return Ok(new { reponse.Messages });
+        //}
+    }
+}
