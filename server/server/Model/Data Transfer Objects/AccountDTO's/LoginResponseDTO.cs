3ee177af3c1c78afa73d22cd856dd8b90a7e62ed
Unknown <aphambi@gmail.com>
2018-04-19 00:35:32 -0700

    Vuex/Login
    vuex-persistedstate
    made User modules in store persist
    created user modules
    Login stuff

--- server/server/Model/Data Transfer Objects/AccountDTO's/LoginResponseDTO.cs
+++ server/server/Model/Data Transfer Objects/AccountDTO's/LoginResponseDTO.cs
@@ -10,5 +10,7 @@ namespace server.Model.Data_Transfer_Objects.AccountDTO_s
     {
         public List<string> Messages { get; set; }
         public bool isSuccessful { get; set; }
+        public string username { get; set; }
+        public string token { get; set; }
     }
 }
