0cbdae32281aa83c1b120834be126927f5f4e354
Unknown <aphambi@gmail.com>
2018-04-19 23:51:15 -0700

    Login
    forgot to push this file

--- server/server/Controllers/LoginController.cs
+++ server/server/Controllers/LoginController.cs
@@ -3,6 +3,9 @@ using server.Model.Data_Transfer_Objects.AccountDTO_s;
 using System;
 using System.Web.Http;
 using System.Web.Http.Cors;
+using Whatfits.DataAccess.DTOs;
+using Whatfits.DataAccess.DTOs.CoreDTOs;
+using Whatfits.DataAccess.Gateways.CoreGateways;
 using Whatfits.JsonWebToken.Controller;
 
 namespace server.Controllers
@@ -19,11 +22,23 @@ namespace server.Controllers
             string username = userCredential.Username;
             response.username = username;
 
+            LoginDTO loginDTO = new LoginDTO()
+            {
+                UserName = username
+            };
+
+            LoginGateway loginGateway = new LoginGateway();
+
+            ResponseDTO<LoginDTO> loginResponseDTO = loginGateway.GetCredentials(loginDTO);
 
 
-            CreateJWT createJWT = new CreateJWT();
-            response.token = createJWT.CreateToken(username); //, "general");
-            return Ok(new { response });
+            if(loginResponseDTO.Data.Type == "Enable")
+            {
+                CreateJWT createJWT = new CreateJWT();
+                response.token = createJWT.CreateToken(username); //, "general");
+            }
+            
+            return Ok(response);
         }
     }
 }
